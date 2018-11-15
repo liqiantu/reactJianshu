99951072f2334102170038f6a1212b5ca9f61008
liqiantu <867899639@qq.com>
2018-11-15 14:55:04 +0800

    header头部完善与input动画添加

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,14 +1,62 @@
 import React, { Component } from 'react';
-import { HeaderWrapper, Logo } from './style'
+import { CSSTransition } from 'react-transition-group'
+import { HeaderWrapper, Logo, Nav, NavItem, NavSearch, SearchWrapper,Action, Button } from './style'
 
 class Header extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            focused: false
+        }
+        this.handleInputBlur = this.handleInputBlur.bind(this);
+        this.handleInputFocus = this.handleInputFocus.bind(this);
+    }
 
     render() {
         return(
             <HeaderWrapper>
                 <Logo />
+                <Nav>
+                    <NavItem className='left active'>首页</NavItem>
+                    <NavItem className='left'>下载App</NavItem>
+                    <NavItem className='right'><i className='iconfont'>&#xe636;</i></NavItem>
+                    <NavItem className='right'>登录</NavItem>
+
+                    <SearchWrapper>
+                        <CSSTransition
+                            in={this.state.focused}
+                            timeout={350}
+                            classNames="slide"
+                        >
+                            <NavSearch 
+                                className={this.state.focused ? 'focused' : ''} 
+                                onFocus={this.handleInputFocus}
+                                onBlur={this.handleInputBlur}
+                            />
+                        </CSSTransition>
+                            <i className={this.state.focused ? 'iconfont focused' : 'iconfont'}>&#xe614;</i>
+                    </SearchWrapper>
+
+                    <Action>
+                        <Button className='reg'>注册</Button>
+                        <Button className='writting'><i className='iconfont'>&#xe615;</i>写文章</Button>
+                    </Action>
+                </Nav>
             </HeaderWrapper>
         )
+    }
+
+    handleInputFocus() {
+        this.setState({
+            focused:true
+        });
+    }
+
+    handleInputBlur() {
+        this.setState({
+            focused:false
+        });
     }
 }
 
--- src/common/header/style.js
+++ src/common/header/style.js
@@ -19,3 +19,109 @@ export const Logo = styled.a.attrs({
     background: url(${logoPic});
     background-size: contain;
 `;
+
+export const Nav = styled.div`
+    width: 960px;
+    margin: 0 auto;
+    height: 100%;
+
+    padding-right: 70px;
+    box-sizing: border-box;
+    /* background: yellow; */
+`;
+
+export const NavItem = styled.div`
+    line-height: 56px;
+    padding: 0 15px;
+    font-size: 17px;
+    color: #333;
+    &.left {
+        float:left;
+    }
+    &.right {
+        float:right;
+        color: #969696;
+    }
+    &.active {
+        color: #ea6f5a;
+    }
+`;
+
+export const SearchWrapper = styled.div`
+    float: left;
+    position: relative;
+    .slide-enter {
+        transition: all .35s ease-out;
+    }
+    .slide-enter-active {
+        width: 240px
+    }
+    .slide-exit {
+        transition: all .35s ease-out;
+    }
+    .slide-exit-active {
+        width: 160px;
+    }
+    .iconfont {
+        position: absolute;
+        right: 5px;
+        bottom: 5px;
+        width: 30px;
+        line-height: 30px;
+        border-radius: 15px;
+        text-align: center;
+        &.focused {
+            background: #777;
+            color: #fff;
+        }
+    }
+`;
+
+export const NavSearch = styled.input.attrs({
+    placeholder: '搜索'
+})`
+    width: 160px;
+    height: 38px;
+    padding: 0 40px 0 10px;
+    margin-top: 9px;
+    margin-left: 20px;
+    box-sizing: border-box;
+    border: none;
+    outline: none;
+    border-radius: 19px;
+    background: #eee;
+    font-size: 14px;
+    color: #666;
+    &::placeholder {
+        color: #999;
+    }
+    &.focused {
+        width: 240px;
+    }
+`;
+
+export const Action = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+    height: 56px;
+`;
+
+export const Button = styled.div`
+    float: right;
+    margin-top: 9px;
+    margin-right: 20px;
+    padding: 0 20px;
+    line-height: 38px;
+    border-radius: 19px;
+    border: 1px solid #ec6149;
+    font-size: 14px;
+    &.reg {
+        color: #ec6149;
+    }
+    &.writting {
+        color: #fff;
+        background: #ec6149; 
+    }
+`;
+
