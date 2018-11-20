4212fdd49dc3dd7df22ab311d276980c9b5d6cc5
liqiantu <867899639@qq.com>
2018-11-20 11:08:07 +0800

    首页 log添加路由

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -3,6 +3,7 @@ import { CSSTransition } from 'react-transition-group'
 import { HeaderWrapper, Logo, Nav, NavItem, NavSearch, SearchWrapper,Action, Button, SearchInfo, SearchInfoTitle, SearchInfoSwitch, SearchInfoItem, SearchInfoList } from './style'
 import { connect } from 'react-redux';
 import { actionCreators } from './store'
+import { Link } from 'react-router-dom'
 
 class Header extends PureComponent {
     
@@ -10,7 +11,9 @@ class Header extends PureComponent {
         const { focused, handleInputBlur, handleInputFocus, list} = this.props;
         return(
             <HeaderWrapper>
-                <Logo />
+                <Link to='/'>
+                    <Logo />
+                </Link>
                 <Nav>
                     <NavItem className='left active'>首页</NavItem>
                     <NavItem className='left'>下载App</NavItem>
--- src/common/header/style.js
+++ src/common/header/style.js
@@ -7,9 +7,20 @@ export const HeaderWrapper = styled.div`
     border-bottom:1px solid #f0f0f0;
 `;
 
-export const Logo = styled.a.attrs({
-    href:'/'
-})`
+// export const Logo = styled.a.attrs({
+//     href:'/'
+// })`
+//     position:absolute;
+//     top: 0;
+//     left: 0;
+//     width: 100px;
+//     height: 56px;
+//     display: block;
+//     background: url(${logoPic});
+//     background-size: contain;
+// `;
+
+export const Logo = styled.div`
     position:absolute;
     top: 0;
     left: 0;
