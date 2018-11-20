f986021b9d6a091d606bbdd95ed0b74cf5b342b9
liqiantu <867899639@qq.com>
2018-11-20 11:43:41 +0800

    detail页面布局

---
+++ src/pages/detail/style.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const DetailWrapper = styled.div`
+    width: 620px;
+    margin: 0 auto;
+`;
+
+export const DetailHeader = styled.div`
+    margin: 50px 0 20px 0;
+    line-height: 44px;
+    font-size: 34px;
+    font-weight: bold;
+    color: #333;
+`;
+
+export const DetailContent = styled.div`
+    color: #2f2f2f;
+    img {
+        width: 100%;
+    }
+
+    p {
+        margin: 25px 0;
+        font-size: 16px;
+    }
+`;
